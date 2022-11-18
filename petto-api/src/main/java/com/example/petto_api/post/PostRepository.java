65cd0b10489cb5adc0e77bbed29d947f4c249a3f
goldffd700@gmail.com <mark0613>
2022-11-18 10:43:44 +0800

    add annotation

--- petto-api/src/main/java/com/example/petto_api/post/PostRepository.java
+++ petto-api/src/main/java/com/example/petto_api/post/PostRepository.java
@@ -1,9 +1,11 @@
 package com.example.petto_api.post;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.ArrayList;
 
-
+@Repository
 public interface PostRepository extends CrudRepository<PostModel, Integer> {
     PostModel findById(int id);
     PostModel findByTitle(String title);
