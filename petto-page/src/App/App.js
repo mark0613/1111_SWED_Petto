80d48b7c8074c13a3d6a528819a192163d748f6b
mark0613 <goldffd700@gmail.com>
2022-12-13 10:57:17 +0800

    update routers

--- petto-page/src/App/App.js
+++ petto-page/src/App/App.js
@@ -3,9 +3,14 @@ import {
     Routes,
     BrowserRouter,
 } from "react-router-dom";
+
+import { PageTemplate } from "../Views/Template";
+import { Register } from "../Views/Register";
+
+import 'antd/dist/antd.min.css';
+
 import { Login } from "../Views/Login";
 import { PostList } from "../Views/Post/PostList";
-import { Register } from "../Views/Register";
 import { Template } from "../Views/Template";
 import { MDCreatePost } from "../Views/Post/MDCreatePost";
 
@@ -14,7 +19,6 @@ import { HomePageNavBarLogin } from "../Components/NavBar/HomePageNavBarLogin";
 import { HomePageNavBarNoLogin } from "../Components/NavBar/HomePageNavBarNoLogin";
 import { OtherPageNavBarLogin } from "../Components/NavBar/OtherPageNavBarLogin";
 import { OtherPageNavBarNoLogin } from "../Components/NavBar/OtherPageNavBarNoLogin";
-import 'antd/dist/antd.min.css';
 
 
 function App() {
@@ -22,12 +26,13 @@ function App() {
         <BrowserRouter>
             <Routes>
                 <Route exact path="/" element={ <></> } />
+                <Route path="/home" element={ <PageTemplate type="home" /> } />
+                <Route path="/register" element={ <Register /> } />
+
                 <Route path="/login" element={ <Login/> } />
                 <Route path="/postList" element={ <PostList/> } />
-                <Route path="/register" element={ <Register/> } />
                 <Route path="/template" element={ <Template/> } />
                 <Route path="/MDCreatePost" element={ <MDCreatePost/> } />
-                
                 <Route path="/CommonCreatePost" element={ <CommonCreatePost/> } />
                 <Route path="/homePageNavBarLogin" element={ <HomePageNavBarLogin/> } />
                 <Route path="/otherPageNavBarLogin" element={ <OtherPageNavBarLogin/> } />
