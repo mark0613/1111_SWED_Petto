537dc722b97dbe961bfb1d22d931e78670e9ae29
enyu <yvonne20151122@gmail.com>
2022-12-15 01:17:55 +0800

    更改MDCreatePost模板

--- petto-page/src/Views/Post/MDCreatePost.js
+++ petto-page/src/Views/Post/MDCreatePost.js
@@ -1,13 +1,11 @@
-import React from 'react'
-import { useState, useEffect } from "react";
+import { React, useState } from "react";
 import {
-    Layout,
+    Avatar,
+    Button,
     Card,
     Form,
     Input,
     Space,
-    Avatar,
-    Button,
     Select,
 } from "antd";
 
@@ -15,24 +13,27 @@ import {
     AuthUtil,
     CookieUtil,
 } from '../../Utils';
-import { OtherPageNavBarLogin } from "../../Components/NavBar/OtherPageNavBarLogin";
+
+import { PageTemplate } from "../Template";
 
 
 const api = "/api/post";
+
 const { TextArea } = Input;
+
 const { Option } = Select;
-var set1;
+
+let set1;
 const handleChange = (value) => {
     console.log(`selected ${value}`);
     set1 = new Set(value);
-    console.log(set1);
 };
+
 const onChange = (e) => {
     console.log('Change:', e.target.value);
 };
 
-function MDCreatePost(props) {
-    const OtherPageNavBarLoginHeaderBlock = props.OtherPageNavBarLoginHeaderBlock;
+function MDCreatePost() {
     const [message, setMessage] = useState(null);
     const [] = useState(false);
     const onFinish = (values) => {
@@ -46,7 +47,6 @@ function MDCreatePost(props) {
         data.append("content", userData.content);
         data.append("mode", userData.mode);
         data.append("tags", userData.tags);
-        console.log(userData);
 
         fetch(
             api,
@@ -59,12 +59,9 @@ function MDCreatePost(props) {
             return response.json();
         })
         .then((data) => {
-
             if (data.hasOwnProperty("jwt")) {
-                console.log(data);
             }
             else {
-                console.log(data);
                 alert(data.message);
                 setMessage(data.message)
                 if (data.message === "建立成功!") {
@@ -76,218 +73,199 @@ function MDCreatePost(props) {
             console.log(`Error: ${error}`);
         })
     };
-    const MDCreatePostContentBlock = (
+    const contentBlock = (
         <>
-            <Layout>
-                <Space
+            <Form
+                name="basic"
+                labelCol={{
+                    span: 8,
+                }}
+                onFinish={ onFinish }
+                wrapperCol={{
+                    span: 16,
+                }}
+            >
+                <Card
                     style={{
-                        marginTop: '30px',
-                        justifyContent: 'center',
+                        width: '910px',
+                        border: '1px solid',
+                        paddingLeft: '20px',
+                        borderRadius: '12px',
                     }}
                 >
-                    <Form
-                        name="basic"
-                        labelCol={{
-                            span: 8,
-                        }}
-                        onFinish={onFinish}
-                        wrapperCol={{
-                            span: 16,
-                        }}
-                    >
-                        <Card
+                    <Form.Item>
+                        <Space
                             style={{
-                                width: '910px',
-                                border: '1px solid',
-                                paddingLeft: '20px',
-                                borderRadius: '12px',
+                                width: '675px',
                             }}
                         >
-                            <Form.Item>
-                                <Space direction="horizontal">
-                                    <Space
-                                        style={{
-                                            width: '675px',
-                                        }}
-                                    >
-                                        <Avatar
-                                            src="https://joeschmoe.io/api/v1/random"
-                                            style={{
-                                                width: '60px',
-                                                height: '60px',
-                                            }}
-                                        />
-
-                                        <h
-                                            style={{
-                                                fontSize: '24px',
-                                            }}
-                                        >
-                                            { AuthUtil.getUserDetails().username }
-                                        </h>
-                                    </Space>
-
-                                    <Space>
-                                        <Button
-                                            type="primary"
-                                            style={{
-                                                borderRadius: '8px',
-                                            }}
-                                            htmlType="submit"
-                                        >
-                                            編輯
-                                        </Button>
-
-                                        <Button
-                                            type="primary"
-                                            style={{
-                                                borderRadius: '8px',
-                                            }}
-                                            htmlType="submit"
-                                        >
-                                            預覽
-                                        </Button>
-                                    </Space>
-                                </Space>
-                            </Form.Item>
-
-                            <Form.Item
-                                name={['user', 'title']}
+                            <Avatar
+                                src="https://joeschmoe.io/api/v1/random"
                                 style={{
-                                    marginBottom: '20px',
+                                    width: '60px',
+                                    height: '60px',
+                                }}
+                            />
+                
+                            <h
+                                style={{
+                                    fontSize: '24px',
                                 }}
                             >
-                                <Input
-                                    style={{
-                                        width: '820px',
-                                        borderColor: 'gray',
-                                        borderWidth: '0 0 1px 0',
-                                    }}
-                                    placeholder="請輸入文章標題"
-                                />
-                            </Form.Item>
-
-                            <Form.Item
-                                name={['user', 'content']}
+                                { AuthUtil.getUserDetails().username }
+                            </h>
+                            
+                            <Button
+                                type="primary"
                                 style={{
-                                    marginBottom: '8px',
+                                    marginLeft: '545px',
+                                    borderRadius: '8px',
                                 }}
+                                htmlType="submit"
                             >
-                                <Space
-                                    style={{
-                                        marginTop: '0px',
-                                    }}
-                                >
-                                    <TextArea
-                                        style={{
-                                            width: '820px',
-                                            height: 320,
-                                            resize: 'none',
-                                            borderColor: 'black',
-                                        }}
-                                        onChange={onChange}
-                                        placeholder="請輸入文章內容.."
-                                    />
-                                </Space>
-                            </Form.Item>
+                                編輯
+                            </Button>
 
-                            <Space
+                            <Button
+                                type="primary"
                                 style={{
-                                    paddingTop: '20px',
+                                    borderRadius: '8px',
                                 }}
-                                direction="horizontal"
+                                htmlType="submit"
                             >
-                                <Form.Item
-                                    name={['user', 'tags']}
-                                    style={{
-                                        marginBottom: '0px',
-                                    }}
-                                >
-                                    <Select
-                                        mode="multiple"
-                                        style={{
-                                            width: '415px',
-                                            borderColor: 'black ',
-                                        }}
-                                        showArrow
-                                        onChange={handleChange}
-                                        placeholder="選擇標籤.."
-                                        optionLabelProp="label"
-                                    >
-                                        <Option value={1} label="犬">
-                                            <div className="demo-option-label-item">
-                                                犬
-                                            </div>
-                                        </Option>
+                                預覽
+                            </Button>  
+                        </Space> 
+                    </Form.Item>
 
-                                        <Option value={2} label="貓">
-                                            <div className="demo-option-label-item">
-                                                貓
-                                            </div>
-                                        </Option>
+                    <Form.Item
+                        name={['user', 'title']}
+                        style={{
+                            width: '1230px',
+                            marginBottom: '20px',
+                        }}
+                    >
+                        <Input
+                            style={{
+                                borderColor: 'gray',
+                                borderWidth: '0 0 1px 0',
+                            }}
+                            placeholder="請輸入文章標題"
+                        />
+                    </Form.Item>
 
-                                        <Option value={3} label="鳥">
-                                            <div className="demo-option-label-item">
-                                                鳥
-                                            </div>
-                                        </Option>
+                    <Form.Item
+                        name={['user', 'content']}
+                        style={{
+                            width: '1500px',
+                            marginBottom: '8px',
+                        }}
+                    >
+                        <TextArea
+                            style={{
+                                width: '820px',
+                                height: 320,
+                                resize: 'none',
+                                borderColor: 'black',
+                            }}
+                            onChange={ onChange }
+                            placeholder="請輸入文章內容.."
+                        />
+                    </Form.Item>
 
-                                        <Option value={4} label="烏龜">
-                                            <div className="demo-option-label-item">
-                                                烏龜
-                                            </div>
-                                        </Option>
-
-                                        <Option value={5} label="守宮">
-                                            <div className="demo-option-label-item">
-                                                守宮
-                                            </div>
-                                        </Option>
-
-                                        <Option value={6} label="吉娃娃">
-                                            <div className="demo-option-label-item">
-                                                吉娃娃
-                                            </div>
-                                        </Option>
-                                    </Select>
-                                </Form.Item>
-
-                                <Form.Item
-                                    style={{
-                                        marginBottom: '0px',
-                                    }}
-                                >
-                                    <div
-                                        style={{
-                                            paddingLeft: '275px',
-                                        }}
-                                    >
-                                        <Button
-                                            type="primary"
-                                            style={{
-                                                width: '120px',
-                                                borderRadius: '8px',
-                                            }}
-                                            onClick={() => Form.submit()}
-                                            htmlType="submit"
-                                        >
-                                            發佈
-                                        </Button>
+                    <Space
+                        style={{
+                            paddingTop: '20px',
+                        }}
+                        direction="horizontal"
+                    >
+                        <Form.Item
+                            name={['user', 'tags']}
+                            style={{
+                                width: '415px',
+                                paddingTop: '20px',
+                                marginBottom: '0px',
+                            }}
+                        >
+                            <Select
+                                mode="multiple"
+                                style={{
+                                    width: '424px',
+                                    borderColor: 'black ',
+                                }}
+                                showArrow
+                                onChange={ handleChange }
+                                placeholder="選擇標籤.."
+                                optionLabelProp="label"
+                            >
+                                <Option value={ 1 } label="犬">
+                                    <div className="demo-option-label-item">
+                                        犬
                                     </div>
-                                </Form.Item>
-                            </Space>
-                        </Card>
-                    </Form>
-                </Space>
-            </Layout>
+                                </Option>
+
+                                <Option value={ 2 } label="貓">
+                                    <div className="demo-option-label-item">
+                                        貓
+                                    </div>
+                                </Option>
+
+                                <Option value={ 3 } label="鳥">
+                                    <div className="demo-option-label-item">
+                                        鳥
+                                    </div>
+                                </Option>
+
+                                <Option value={ 4 } label="烏龜">
+                                    <div className="demo-option-label-item">
+                                        烏龜
+                                    </div>
+                                </Option>
+
+                                <Option value={ 5 } label="守宮">
+                                    <div className="demo-option-label-item">
+                                        守宮
+                                    </div>
+                                </Option>
+
+                                <Option value={ 6 } label="吉娃娃">
+                                    <div className="demo-option-label-item">
+                                        吉娃娃
+                                    </div>
+                                </Option>
+                            </Select>
+                        </Form.Item>
+
+                        <Form.Item
+                            style={{
+                                marginBottom: '0px',
+                            }}
+                        >
+                            <h
+                                style={{
+                                    paddingLeft: '275px',
+                                }}
+                            >
+                                <Button
+                                    type="primary"
+                                    style={{
+                                        width: '120px',
+                                        borderRadius: '8px',
+                                    }}
+                                    onClick={ () => Form.submit() }
+                                    htmlType="submit"
+                                >
+                                    發佈
+                                </Button>
+                            </h>
+                        </Form.Item>
+                    </Space>
+                </Card>
+            </Form>
         </>
     )
-
     return (
-        <OtherPageNavBarLogin
-            MDCreatePostContentBlock={MDCreatePostContentBlock}
-            OtherPageNavBarLoginHeaderBlock={OtherPageNavBarLoginHeaderBlock}
-        />
+        <PageTemplate contentBlock={ contentBlock } />
     )
 };
 
