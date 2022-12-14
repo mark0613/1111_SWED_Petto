05806afb1549d44605e9e5e9dfb00dd2bd5f7781
enyu <yvonne20151122@gmail.com>
2022-12-14 23:46:55 +0800

    更改PostList模板

--- petto-page/src/Views/Post/PostList.js
+++ petto-page/src/Views/Post/PostList.js
@@ -1,31 +1,30 @@
-import { useState, useEffect, } from "react";
 import {
+    React,
+    useState,
+    useEffect
+} from "react";
+import {
+    Avatar,
+    Button,
+    BackTop,
     Col,
-    Row,
-    Tag,
     Card,
     Form,
-    Input,
+    Row,
     Space,
-    Button,
-    Layout,
-    Select,
-    Avatar,
-    BackTop,
     Statistic,
     Typography,
 } from "antd";
-import 'antd/dist/antd.css';
 import { MessageOutlined } from '@ant-design/icons';
-import React from 'react'
-import { HomePageNavBarLogin } from "../../Components/NavBar/HomePageNavBarLogin";
-import { HomePageNavBarNoLogin } from "../../Components/NavBar/HomePageNavBarNoLogin";
-import { AuthUtil } from '../../Utils'
+
+import { PageTemplate } from "../Template";
+import 'antd/dist/antd.css';
 import "./PostList.css"
 
 
-const api = "/api/posts";
 const { Paragraph, Title } = Typography;
+
+const api = "/api/posts";
 
 function fetchApi(api, callback) {
     fetch(api)
@@ -55,56 +54,50 @@ function getCard(post) {
                 marginBottom: '40px',
             }}
         >
-            <Form.Item >
-                <Space direction="horizontal">
-                    <Space>
-                        <Avatar
-                            style={{
-                                width: '60px',
-                                height: '60px',
-                            }}
-                            src="https://joeschmoe.io/api/v1/random"
-                        />
+            <Form.Item direction="horizontal">
+                <Avatar
+                    style={{
+                        width: '60px',
+                        height: '60px',
+                    }}
+                    src="https://joeschmoe.io/api/v1/random"
+                />
 
-                        <h
-                            style={{
-                                fontSize: '24px',
-                            }}
-                        >
-                            { post.username }
-                        </h>
+                <h
+                    style={{
+                        fontSize: '24px',
+                    }}
+                >
+                    { post.username }
+                </h>
 
-                        <p
-                            style={{
-                                color: 'lightgray',
-                                fontSize: '20px',
-                                marginLeft:'30px',
-                                marginBottom:'0px',
-                            }}
-                        >
-                            { post.timestamp.substring(0, 10) }
-                        </p>
-
-                    </Space>
-                </Space>
+                <h
+                    style={{
+                        color: 'lightgray',
+                        fontSize: '20px',
+                        marginLeft: '30px',
+                        marginBottom: '0px',
+                    }}
+                >
+                    { post.timestamp.substring(0, 10) }
+                </h>
             </Form.Item>
+
             <Form.Item>
-                <Space direction="vertical">
-                    <Title level={3}>{ post.title }</Title>
-                    <Paragraph
-                        style={{
-                            width: '820px',
-                            fontSize: '18px',
-                        }}
-                        ellipsis={{
-                            rows: 3,
-                        }}
-                        onClick={() => window.location.href = '/Login'}
-                        
-                    >
-                        <div dangerouslySetInnerHTML={{__html: post.content}} />
-                    </Paragraph>
-                </Space>
+                <Title level={3}>{ post.title }</Title>
+
+                <Paragraph
+                    style={{
+                        width: '820px',
+                        fontSize: '18px',
+                    }}
+                    ellipsis={{
+                        rows: 3,
+                    }}
+                    onClick={ () => window.location.href = '/Login' }
+                >
+                    <div dangerouslySetInnerHTML={{ __html: post.content }} />
+                </Paragraph>
             </Form.Item>
 
             <Form.Item>
@@ -119,16 +112,10 @@ function getCard(post) {
 
                     <Row>
                         <Statistic
-                           value={ countEmoji }
+                            value={ countEmoji }
                         />
                     </Row>
-                </Space>
 
-                <Space
-                    style={{
-                        marginLeft: '70px',
-                    }}
-                >
                     <Row>
                         <Col>
                             <Statistic
@@ -139,22 +126,17 @@ function getCard(post) {
                     </Row>
                 </Space>
 
-                <Space>
-                    <BackTop />
-                    <strong className="site-back-top-basic"></strong>
-                </Space>
+                <BackTop />
+
+                <strong className="site-back-top-basic"></strong>
             </Form.Item>
         </Card>
     )
 }
 
-function PostList(props) {
+function PostList() {
     const [data, setData] = useState(null);
     const [list, setList] = useState([]);
-    const [countEmoji, setcountEmoji] = useState([]);
-    const OtherPageNavBarLoginHeaderBlock = props.OtherPageNavBarLoginHeaderBlock;
-    const HomePageNavBarLoginHeaderBlock = props.HomePageNavBarLoginHeaderBlock;
-    const HomePageNavBarNoLoginHeaderBlock = props.HomePageNavBarNoLoginHeaderBlock;
     const [] = useState(false);
     useEffect(() => {
         fetchApi(api, (resp) => {
@@ -164,90 +146,69 @@ function PostList(props) {
             }
         });
     }, []);
-    const PostListContentBlock = (
+    const contentBlock = (
         <>
-            <Layout>
-                <Space
+            <Form
+                name="basic"
+                labelCol={{
+                    span: 8,
+                }}
+                wrapperCol={{
+                    span: 16,
+                }}
+            >
+                <Form.Item
                     style={{
-                        marginTop: '30px',
+                        display: 'flex',
                         justifyContent: 'center',
                     }}
                 >
-                    <Form
-                        name="basic"
-                        labelCol={{
-                            span: 8,
-                        }}
-                        wrapperCol={{
-                            span: 16,
-                        }}
-                    >
-                        <Form.Item
+                    <Space>
+                        <Button
+                            type="primary"
                             style={{
-                                display: 'flex',
-                                justifyContent: 'center',
+                                width: '120px',
+                                borderRadius: '8px',
                             }}
+                            onClick={ () => window.location.href = '/CommonCreatePost' }
+                            htmlType="submit"
                         >
-                            <Space>
-                                <Button
-                                    type="primary"
-                                    style={{
-                                        width: '120px',
-                                        borderRadius: '8px',
-                                    }}
-                                    onClick={() => window.location.href = '/CommonCreatePost'}
-                                    htmlType="submit"
-                                >
-                                    建立一般文章
-                                </Button>
+                            建立一般文章
+                        </Button>
 
-                                <Button
-                                    type="primary"
-                                    style={{
-                                        width: '120px',
-                                        borderRadius: '8px',
-                                    }}
-                                    onClick={() => window.location.href = '/MDCreatePost'}
-                                    htmlType="submit"
-                                >
-                                    建立MD文章
-                                </Button>
+                        <Button
+                            type="primary"
+                            style={{
+                                width: '120px',
+                                borderRadius: '8px',
+                            }}
+                            onClick={ () => window.location.href = '/MDCreatePost' }
+                            htmlType="submit"
+                        >
+                            建立MD文章
+                        </Button>
 
-                                <Button
-                                    type="primary"
-                                    style={{
-                                        width: '120px',
-                                        borderRadius: '8px',
-                                    }}
-                                    onClick={() => window.location.href = '/HomePageNavBarLogin'}
-                                    htmlType="submit"
-                                >
-                                    建立投票
-                                </Button>
-                            </Space>
-                        </Form.Item>
+                        <Button
+                            type="primary"
+                            style={{
+                                width: '120px',
+                                borderRadius: '8px',
+                            }}
+                            onClick={ () => window.location.href = '/HomePageNavBarLogin' }
+                            htmlType="submit"
+                        >
+                            建立投票
+                        </Button>
+                    </Space>
+                </Form.Item>
 
-                        {list}
-                    </Form>
-                </Space>
-            </Layout>
+                { list }
+            </Form>
         </>
     )
-    if (AuthUtil.isLogin()) {
-        return (
-            <HomePageNavBarLogin
-                PostListContentBlock={PostListContentBlock}
-                HomePageNavBarLoginHeaderBlock={HomePageNavBarLoginHeaderBlock}
-            />
-        )
-    } else {
-        return (
-            <HomePageNavBarNoLogin
-                PostListContentBlock={PostListContentBlock}
-                HomePageNavBarNoLoginHeaderBlock={HomePageNavBarNoLoginHeaderBlock}
-            />
-        )
-    }
-};
+    return (
+        <PageTemplate contentBlock={ contentBlock } />
+    )
+}
 
 export { PostList };
