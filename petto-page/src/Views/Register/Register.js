18a95a3d2432f0ab1cca66afd18f4ca71a44345f
mark0613 <goldffd700@gmail.com>
2022-12-17 00:49:16 +0800

    feat: 調整頁面

--- petto-page/src/Views/Register/Register.js
+++ petto-page/src/Views/Register/Register.js
@@ -1,8 +1,10 @@
 import {
     Button,
     Card,
+    Col,
     Form,
     Input,
+    Row,
     Typography,
 } from "antd";
 
@@ -48,112 +50,118 @@ const onFinish = (data) => {
 function Register() {
     const contentBlock = (
         <>
-            <Card 
-                style={{
-                    borderRadius: 20,
-                    padding: '3%',
-                    backgroundColor: '#edf8ff',
-                }}
-            >
-                <Title 
-                    style={{
-                        color: '#4691ee',
-                        fontSize: '40px',
-                        textAlign: 'center',
-                    }}
-                    level={ 3 }
-                >
-                    註冊
-                </Title>
-                    
-                <Form 
-                    labelCol={{ span: 10 }}
-                    onFinish={ onFinish } 
-                    validateMessages={ validateMessages }
-                >
-                    <Form.Item 
-                        name={['user', 'email']} 
-                        label="Email" 
-                        rules={[
-                            {
-                                type: 'email' 
-                            }
-                        ]}
-                    > 
-                        <Input 
-                            style={{
-                                width:'250px',
-                            }}
-                            placeholder="請輸入帳號...." 
-                        />
-                    </Form.Item>
-
-                    <Form.Item 
-                        name={['user', 'username']}
-                        label="Username"
-                        rules={[
-                            {
-                                message: 'Please enter the username!',
-                                required: true,
-                            },
-                        ]}
-                    >
-                        <Input 
-                            style={{
-                                width:'250px',
-                            }}
-                            placeholder="請輸入名稱...."
-                        />
-                    </Form.Item>
-
-                    <Form.Item 
-                        name={['user', 'password']} 
-                        label="Password"
-                        rules={[
-                            {
-                                message: 'Please enter the password!',
-                                required: true,
-                            },
-                        ]}
-                    >
-                        <Input.Password 
-                            style={{
-                                width:'250px',
-                            }}
-                            placeholder="請輸入密碼...."
-                        />
-                    </Form.Item>
-
-                    <Form.Item 
+            <Row>
+                <Col span={ 6 }></Col>
+                <Col span={ 12 }>
+                    <Card 
                         style={{
-                            textAlign: "center",
+                            borderRadius: 20,
+                            padding: '3%',
+                            backgroundColor: '#edf8ff',
                         }}
                     >
-                        <Button 
-                            type="primary" 
-                            htmlType="submit"
-                            onClick={ () => Form.submit()}
+                        <Title 
+                            style={{
+                                color: '#4691ee',
+                                fontSize: '40px',
+                                textAlign: 'center',
+                            }}
+                            level={ 3 }
                         >
-                            立即註冊
-                        </Button>
-                    </Form.Item>
-                </Form>
+                            註冊
+                        </Title>
+                            
+                        <Form 
+                            labelCol={{ span: 10 }}
+                            onFinish={ onFinish } 
+                            validateMessages={ validateMessages }
+                        >
+                            <Form.Item 
+                                name={['user', 'email']} 
+                                label="Email" 
+                                rules={[
+                                    {
+                                        type: 'email' 
+                                    }
+                                ]}
+                            > 
+                                <Input 
+                                    style={{
+                                        width:'250px',
+                                    }}
+                                    placeholder="請輸入帳號...." 
+                                />
+                            </Form.Item>
 
-                <div style={{ textAlign: "center" }}>
-                    已有帳號?
-                    <Button
-                        type="link"
-                        size="20px"
-                        style={{
-                            padding:'0px 0px 16px 2px',
-                        }}
-                    >
-                        <a href="/login">
-                            <u>立即登入</u>
-                        </a>
-                    </Button>
-                </div>
-            </Card>
+                            <Form.Item 
+                                name={['user', 'username']}
+                                label="Username"
+                                rules={[
+                                    {
+                                        message: 'Please enter the username!',
+                                        required: true,
+                                    },
+                                ]}
+                            >
+                                <Input 
+                                    style={{
+                                        width:'250px',
+                                    }}
+                                    placeholder="請輸入名稱...."
+                                />
+                            </Form.Item>
+
+                            <Form.Item 
+                                name={['user', 'password']} 
+                                label="Password"
+                                rules={[
+                                    {
+                                        message: 'Please enter the password!',
+                                        required: true,
+                                    },
+                                ]}
+                            >
+                                <Input.Password 
+                                    style={{
+                                        width:'250px',
+                                    }}
+                                    placeholder="請輸入密碼...."
+                                />
+                            </Form.Item>
+
+                            <Form.Item 
+                                style={{
+                                    textAlign: "center",
+                                }}
+                            >
+                                <Button 
+                                    type="primary" 
+                                    htmlType="submit"
+                                    onClick={ () => Form.submit()}
+                                >
+                                    立即註冊
+                                </Button>
+                            </Form.Item>
+                        </Form>
+
+                        <div style={{ textAlign: "center" }}>
+                            已有帳號?
+                            <Button
+                                type="link"
+                                size="20px"
+                                style={{
+                                    padding:'0px 0px 16px 2px',
+                                }}
+                            >
+                                <a href="/login">
+                                    <u>立即登入</u>
+                                </a>
+                            </Button>
+                        </div>
+                    </Card>
+                </Col>
+                <Col span={ 6 }></Col>
+            </Row>
         </>
     )
     return (
