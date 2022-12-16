e84e57e5dfa563463541f3885530c9405a266dc0
mark0613 <goldffd700@gmail.com>
2022-12-16 09:22:00 +0800

    refactor: 清理用不到的檔案

--- petto-page/src/App/App.js
+++ petto-page/src/App/App.js
@@ -6,38 +6,26 @@ import {
 
 import { PageTemplate } from "../Views/Template";
 import { Register } from "../Views/Register";
-
-import 'antd/dist/antd.min.css';
-
 import { Login } from "../Views/Login";
 import { PostList } from "../Views/Post/PostList";
-import { Template } from "../Views/Template";
 import { MDCreatePost } from "../Views/Post/MDCreatePost";
-
 import { CommonCreatePost } from "../Views/Post/CommonCreatePost";
-import { HomePageNavBarLogin } from "../Components/NavBar/HomePageNavBarLogin";
-import { HomePageNavBarNoLogin } from "../Components/NavBar/HomePageNavBarNoLogin";
-import { OtherPageNavBarLogin } from "../Components/NavBar/OtherPageNavBarLogin";
-import { OtherPageNavBarNoLogin } from "../Components/NavBar/OtherPageNavBarNoLogin";
+
+import 'antd/dist/antd.min.css';
 
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route exact path="/" element={ <></> } />
+                <Route exact path="/" element={ <PageTemplate type="home" /> } />
                 <Route path="/home" element={ <PageTemplate type="home" /> } />
                 <Route path="/register" element={ <Register /> } />
-
                 <Route path="/login" element={ <Login/> } />
+
                 <Route path="/postList" element={ <PostList/> } />
-                <Route path="/template" element={ <Template/> } />
                 <Route path="/MDCreatePost" element={ <MDCreatePost/> } />
                 <Route path="/CommonCreatePost" element={ <CommonCreatePost/> } />
-                <Route path="/homePageNavBarLogin" element={ <HomePageNavBarLogin/> } />
-                <Route path="/otherPageNavBarLogin" element={ <OtherPageNavBarLogin/> } />
-                <Route path="/homePageNavBarNoLogin" element={ <HomePageNavBarNoLogin/> } />
-                <Route path="/otherPageNavBarNoLogin" element={ <OtherPageNavBarNoLogin/> } />
                 <Route path="*" element={ <></> } />
             </Routes>
         </BrowserRouter>
