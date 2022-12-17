2d40d4385ec7cf8a9035fded7fae5a40ea77e1f2
mark0613 <goldffd700@gmail.com>
2022-12-17 11:44:39 +0800

    refactor: 使用 universal-cookie 重構

--- petto-page/src/Utils/CookieUtil.js
+++ petto-page/src/Utils/CookieUtil.js
@@ -1,47 +1,27 @@
-class CookieUtil {
-    static getAllCookies() {
-        let result = {};
-        if (document.cookie.length === 0) {
-            return result;
-        }
-        document.cookie.split("; ").map((pair) => {
-            let [k, v] = pair.split("=");
-            result[k] = v;
-        });
-        return result;
-    }
+import Cookies from 'universal-cookie';
 
-    static replaceAll(cookiesJson) {
-        let result = [];
-        for (let k in cookiesJson) {
-            let v = cookiesJson[k];
-            result.push(`${k}=${v}`);
-        }
-        if (result.length > 0) {
-            document.cookie = result.join("; ");
-        }
+
+const cookies = new Cookies();
+
+class CookieUtil {
+    static set(key, value) {
+        cookies.set(
+            key,
+            value, 
+            { 
+                path: '/',
+                secure: true,
+                sameSite :true
+            }
+        );
     }
 
     static getValue(key) {
-        return CookieUtil.getAllCookies()[key];
-    }
-
-    static set(key, value) {
-        let all = CookieUtil.getAllCookies();
-        all[key] = value;
-        CookieUtil.replaceAll(all);
+        return cookies.get(key);
     }
 
     static contains(key) {
-        return CookieUtil.getAllCookies().hasOwnProperty(key);
-    }
-
-    static clearAll() {
-        let keys = document.cookie.match(/[^ =;]+(?=\=)/g);
-        if(keys) {
-            for(var i = keys.length;i--;)
-            document.cookie = keys[i] + '=0;expires=' + new Date(0).toUTCString()
-        }
+        return cookies.getAll().hasOwnProperty(key);
     }
 }
 
