4234d7e664c2b0792eed4998dc4a1ceb654f5162
Eating0904 <alien1128love@gmail.com>
2022-12-18 10:02:52 +0800

    feat: 刪除文章

---
+++ petto-page/src/Views/Post/Delete.js
@@ -0,0 +1,42 @@
+import { 
+    DeleteOutlined,
+} from '@ant-design/icons';
+import { 
+    AuthUtil,
+    CookieUtil,
+    Request,
+} from "../../Utils";
+
+function onClick(id) {
+    let formData = new FormData();
+    formData.append("jwt", CookieUtil.getValue("token"));
+    
+    Request.delete(
+        `/api/post/${id}`,
+        {
+            body : formData,
+            success : (response) => {
+                console.log(response);
+                alert(response.message);
+                window.location.href='/';
+            }
+        }
+    )
+}
+function DeleteBtn(props) {
+    let name = AuthUtil.getUserDetails().username;
+    if (props.username === name) {
+        return (
+            <DeleteOutlined 
+                onClick={ () => onClick(props.id) }
+            />
+        )
+    }
+    else {
+        return (
+            <></>
+        )
+    }
+}
+
+export { DeleteBtn }
