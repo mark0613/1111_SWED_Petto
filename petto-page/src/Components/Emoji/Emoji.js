607dc835faf893780e7b14ed1eda35c1317c1ead
Eating0904 <alien1128love@gmail.com>
2022-12-19 00:20:44 +0800

    修改圖片路徑

--- petto-page/src/Components/Emoji/Emoji.js
+++ petto-page/src/Components/Emoji/Emoji.js
@@ -4,11 +4,11 @@ import {
 
 
 const emojiUrl = {
-    good : "https://cdn-icons-png.flaticon.com/512/4926/4926585.png",
-    love : "https://cdn-icons-png.flaticon.com/512/742/742750.png",
-    funny : "https://cdn-icons-png.flaticon.com/512/742/742920.png",
-    sad : "https://cdn-icons-png.flaticon.com/512/742/742784.png",
-    angry : "https://cdn-icons-png.flaticon.com/512/743/743418.png",
+    good : `${process.env.PUBLIC_URL}/images/good.png`,
+    love : `${process.env.PUBLIC_URL}/images/love.png`,
+    funny : `${process.env.PUBLIC_URL}/images/funny.png`,
+    sad : `${process.env.PUBLIC_URL}/images/sad.png`,
+    angry : `${process.env.PUBLIC_URL}/images/angry.png`,
 };
 
 function getAllEmojis() {
