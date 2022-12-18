9a0a2932e84cabe9a2801c373e251e0fe573e1d1
mark0613 <goldffd700@gmail.com>
2022-12-18 17:39:53 +0800

    feat: get user voting option in a post if user has voted

--- petto-api/src/main/java/com/example/petto_api/vote/VoteController.java
+++ petto-api/src/main/java/com/example/petto_api/vote/VoteController.java
@@ -1,5 +1,7 @@
 package com.example.petto_api.vote;
 
+import com.example.petto_api.post.PostModel;
+import com.example.petto_api.post.PostService;
 import com.example.petto_api.security.JwtTokenService;
 import com.example.petto_api.user.UserModel;
 import com.example.petto_api.user.UserService;
@@ -7,11 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 
 @Controller
@@ -21,10 +26,44 @@ public class VoteController {
     private JwtTokenService jwtTokenService;
 
     @Autowired
+    private PostService postService;
+
+    @Autowired
     private UserService userService;
 
     @Autowired
     private VoteService voteService;
+
+    @GetMapping("/vote-record")
+    public ResponseEntity<Map<String, Object>> userVoting(@RequestParam int post, @RequestParam int user) {
+        Map<String, Object> response = new HashMap<>();
+        PostModel p = postService.getPostById(post);
+        UserModel u = userService.getUserById(user);
+        if (p == null || u == null) {
+            response.put("option", null);
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        }
+
+        if (!p.getMode().equals("vote")) {
+            response.put("option", null);
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        }
+
+        Set<VoteModel> userVotingRecord = voteService.getUserVoting(u);
+        if (userVotingRecord.size() == 0) {
+            response.put("option", null);
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        }
+
+        for (VoteModel record : userVotingRecord) {
+            if (record.getPost().getId() == p.getId()) {
+                response.put("option", record);
+                return ResponseEntity.status(HttpStatus.OK).body(response);
+            }
+        }
+        response.put("option", null);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
 
     @PostMapping("/vote")
     public ResponseEntity<Map<String, Object>> vote(VoteRequest request) {
